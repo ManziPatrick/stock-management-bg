37bcac0b58717e0ccaa0ffa72c9ea435f1759466
ManziPatrick <alainmanzi71gmail.com>
2025-01-07 17:42:47 +0200

    mod

--- src/app.ts
+++ src/app.ts
@@ -10,7 +10,7 @@ const app: Application = express();
 app.use(express.json());
 app.use(morgan('dev'));
 
-app.use(cors({ origin: ['http://localhost:5173', ''] }));
+app.use(cors({ origin: ['http://localhost:5173', 'https://store-managementdemo.netlify.app'] }));
 
 // application routes
 app.use('/api/v1', rootRouter);
