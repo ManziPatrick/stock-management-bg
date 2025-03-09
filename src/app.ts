8f70631f4241d544e604cabef5cc76b3beb1b39c
Alice Mukarwema <157026426+alicemukarwema@users.noreply.github.com>
2025-03-09 13:05:34 +0200

    update cors

--- src/app.ts
+++ src/app.ts
@@ -7,10 +7,15 @@ import globalErrorHandler from './middlewares/globalErrorhandler';
 
 const app: Application = express();
 
+// This allows preflight requests and sets appropriate headers
+app.use(cors({
+  origin: '*', // Allow all origins
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allow these HTTP methods
+  allowedHeaders: ['Content-Type', 'Authorization'] // Allow these headers
+}));
+
 app.use(express.json());
 app.use(morgan('dev'));
-
-app.use(cors());
 
 // application routes
 app.use('/api/v1', rootRouter);
