5362dc9d620c35a333dbcc6a37263db029fb94fc
ManziPatrick <144239912+ManziPatrick@users.noreply.github.com>
2025-04-14 15:25:16 -0700

    Delete dist directory

--- dist/errors/castError.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const castError = () => {
-    return { _id: 'Request ID is invalid!' };
-};
-exports.default = castError;
