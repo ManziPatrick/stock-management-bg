4d31f82ad133b955a0c8f899083eae833e899257
Alain Manzi <munyeshurimanzi@gmail.com>
2025-05-03 13:40:04 +0200

    good

--- src/modules/debits/debits.service.ts
+++ src/modules/debits/debits.service.ts
@@ -12,7 +12,9 @@ export class DebitService {
 
   constructor() {
     this.transporter = nodemailer.createTransport({
-      service: 'gmail',
+      host: 'smtp.zoho.com',
+      port: 465,
+      secure: true,
       auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.EMAIL_PASS
